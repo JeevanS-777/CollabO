9974340f0be07fbd8db3c2d9249989a2756dc0b8
Jeevan S <jeevans23062005@gmail.com>
2026-03-02 21:53:58 +0530

    Fixed socket CORS for vercel production

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [ENV.CLIENT_URL],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://collab-o-eta.vercel.app"
+    ],
     credentials: true,
   },
 });
