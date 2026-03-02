e12cac1e5f3d1e3f1303bcb9a2b37f2163beb9f1
Jeevan S <jeevans23062005@gmail.com>
2026-03-02 20:15:38 +0530

    Fixed auth cookie cross origin

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,14 +12,11 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, 
+    secure: ENV.NODE_ENV === "production",
+    sameSite: ENV.NODE_ENV === "production" ? "none" : "lax",
   });
 
   return token;
 };
-
-// http://localhost
-// https://dsmakmk.com
