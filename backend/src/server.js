2b31fb39620165fbc318a20a346858c40766ade5
Jeevan S <jeevans23062005@gmail.com>
2026-03-02 19:06:57 +0530

    Prepare backend for render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,16 +9,16 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-const PORT = ENV.PORT || 3000;
+const PORT = ENV.PORT || 5000;
 
 app.use(express.json({ limit: "5mb" })); // req.body
 app.set("trust proxy", 1);
 // app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(
   cors({
-    origin: [ENV.CLIENT_URL],
+    // origin: [ENV.CLIENT_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
@@ -29,13 +29,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
